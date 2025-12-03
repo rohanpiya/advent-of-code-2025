97f91629223035d829c4664957bbe7bc18f905b4
rohanpiya <piyar@dickinson.edu>
2025-12-02 20:23:33 -0500

    Attempt to solve part 1 of day 2 puzzle. Unsuccessful :(

---
+++ Day2/solution.py
@@ -0,0 +1,28 @@
+def isInvalidId(n):
+    s = str(n)
+    if len(s) % 2 !=0:
+        return False
+    half = len(s) // 2
+    return s[:half] == s[half:] 
+
+ranges = []
+
+with open("input.txt", "r") as f:
+    line = f.read().strip()
+    rangeList = line.split(',')
+
+    for r in rangeList:
+        startEndList = r.split('-')
+        ranges.append([int(startEndList[0]), int(startEndList[1])])
+
+totalInvalidSum = 0
+
+for start,end in ranges:
+    for num in range(start, end+1):
+        if isInvalidId(num):
+            totalInvalidSum += 1
+
+print(f"Answer: {totalInvalidSum}")
+
+# print("Ranges Parsed: ", len(ranges))
+# print(ranges)
